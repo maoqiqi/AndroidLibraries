2ee93c7c75290f3620d5dcc1bb0c7b68dd0ab719
maoqiqi <mao_fengqi@163.com>
2021-02-23 18:24:42 +0800

    IntentUtils

--- lib-android/src/main/java/com/codearms/maoqiqi/android/IntentUtils.kt
+++ lib-android/src/main/java/com/codearms/maoqiqi/android/IntentUtils.kt
@@ -1,8 +1,26 @@
 package com.codearms.maoqiqi.android
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Bundle
 
-fun Context.isIntentAvailable(intent: Intent) =
-    packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).size > 0
+object IntentUtils {
+
+    @JvmStatic
+    fun Context.isIntentAvailable(intent: Intent) =
+        packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).size > 0
+
+    @JvmStatic
+    @JvmOverloads
+    fun <T : Activity> Context.startActivity(clazz: Class<out T>, extras: Bundle? = null, options: Bundle? = null) {
+        startActivity(createIntent(clazz, extras), options)
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun <T : Activity> Context.createIntent(clazz: Class<out T>, extras: Bundle? = null): Intent {
+        return Intent(this, clazz).apply { extras?.let { putExtras(it) } }
+    }
+}
